53edf6955d2dc036f978a6e4190ab16e7527bccc
evan <cppmain@gmail.com>
2019-01-26 17:31:43 +0800

    init
    Signed-off-by: evan <cppmain@gmail.com>

---
+++ first/src/hello/com/buyou/hello/Hello.java
@@ -0,0 +1,9 @@
+package com.buyou.hello;
+
+import java.util.Date;
+
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Hello World! " + new Date());
+	}
+}
---
+++ first/src/hello/module-info.java
@@ -0,0 +1,3 @@
+module hello {
+	exports com.buyou.hello;
+}
